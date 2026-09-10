f9ea8d5c694dbef1ac6e8a8ab0ca16b33dfbff48
Calliste Godailler <calliste.godailler@gmail.com>
2026-09-10 12:07:30 +0200

    app icon

--- vue/v_entete.php
+++ vue/v_entete.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="format-detection" content="telephone=no">
+    <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' 
+    viewBox='0 0 64 64' fill='none'><rect x='22' y='6' width='20' height='52' rx='4' fill='%235882C7'/><rect 
+    x='6' y='22' width='52' height='20' rx='4' fill='%235882C7'/><path d='M32 20C32 20 24 30 24 36C24 40.4183 27.5817 44 32 
+    44C36.4183 44 40 40.4183 40 36C40 30 32 20 32 20Z' fill='white'/></svg>">
     <title>LymphTrack</title>
     <style>
         * {
